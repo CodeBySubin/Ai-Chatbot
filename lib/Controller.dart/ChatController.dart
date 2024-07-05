67e49acb78ccaf298083090cdbd77df128194077
subin <subin.sigosoft@gmail.com>
2024-07-05 16:17:09 +0530

    speech to text web issues solved

--- lib/Controller.dart/ChatController.dart
+++ lib/Controller.dart/ChatController.dart
@@ -14,7 +14,6 @@ class ChatController extends GetxController {
   final TextEditingController chatController = TextEditingController();
   final ScrollController scrollController = ScrollController();
   var chatHistory = <ChatModel>[].obs;
-  String? file;
   late Box<ChatModel> chatBox;
   final flutterTts = FlutterTts();
   late final GenerativeModel model;
@@ -22,67 +21,19 @@ class ChatController extends GetxController {
   late final ChatSession chat;
   final SpeechToText speechToText = SpeechToText();
   bool isListening = false;
+  bool isSpeaking = false;
+
   String lastWords = '';
-
-  Future<void> initSpeechToText() async {
-    await speechToText.initialize(
-      onError: (error) => showSnackbar(error.toString()),
-      onStatus: (status) {
-        if (status == 'done') {
-          print("done");
-          sendMessage();
-        }
-      },
-    );
-  }
-
-  Future<void> startListening() async {
-    await speechToText.listen(
-      onResult: onSpeechResult,
-      listenFor: const Duration(seconds: 5),
-    );
-  }
-
-  Future<void> stopListening() async {
-    await speechToText.stop();
-  }
-
-  void onSpeechResult(SpeechRecognitionResult result) {
-    lastWords = result.recognizedWords;
-    chatController.text = lastWords;
-    update();
-  }
-
-  Future<void> initTextToSpeech() async {
-    await flutterTts.setSharedInstance(true);
-    update();
-  }
-
-  Future<void> systemSpeak(String content) async {
-    await flutterTts.setVoice({
-       'rate': '0.8', // Adjusted speech rate for a deliberate, measured pace
-      'pitch': '1.2', // Higher pitch for a more feminine sound
-      'volume': '1.0', // Full volume level
-      'name': 'en-IN-Standard-A', // Voice name or language code (Indian English female voice)
-      'locale': 'en-IN', // Language locale (for Indian English)
-    });
-    await flutterTts.speak(content);
-  }
-
-  @override
-  void onClose() {
-    speechToText.stop();
-    chatController.dispose();
-    scrollController.dispose();
-    flutterTts.stop();
-    super.onClose();
-  }
 
   @override
   void onInit() {
     super.onInit();
     initSpeechToText();
     initTextToSpeech();
+    flutterTts.setCompletionHandler(() {
+      isSpeaking = false;
+      update();
+    });
     model = GenerativeModel(
       model: 'gemini-pro',
       apiKey: 'AIzaSyA5WsZn0bhGR5hHmDMshhBtU2_Itw5FBRY',
@@ -99,52 +50,94 @@ class ChatController extends GetxController {
     });
   }
 
-  void pickFile() async {
-    FilePickerResult? result = await FilePicker.platform.pickFiles(
-      type: FileType.custom,
-      allowedExtensions: ['jpg', 'jpeg', 'png'],
+  Future<void> initSpeechToText() async {
+    await speechToText.initialize(
+      options: [SpeechToText.webDoNotAggregate],
+      onError: (error) => showSnackbar(error.toString()),
+      onStatus: (status) {
+        if (status == 'done') {
+          isListening = false;
+          update();
+          sendMessage();
+        }
+      },
     );
-    if (result != null) {
-      file = result.files.first.path;
+  }
+
+  Future<void> startListening() async {
+    if (!isListening) {
+      isListening = true;
+      await speechToText.listen(
+        onResult: onSpeechResult,
+        listenFor: const Duration(seconds: 5),
+      );
+    }
+  }
+
+  void onSpeechResult(SpeechRecognitionResult result) {
+    chatController.text = result.recognizedWords;
+  }
+
+  Future<void> stopListening() async {
+    if (isListening) {
+      isListening = false;
+      await speechToText.stop();
       update();
     }
   }
 
+  Future<void> initTextToSpeech() async {
+    await flutterTts.setSharedInstance(true);
+    update();
+  }
+
+  Future<void> systemSpeak(String content) async {
+    isSpeaking = true;
+    flutterTts.setSpeechRate(1.0);
+    flutterTts.setPitch(1.0);
+    flutterTts.setVolume(1.0);
+    flutterTts.setVoice({'name': 'en-US-Wavenet-D', 'locale': 'en-US'});
+    await flutterTts.speak(content);
+  }
+
+  @override
+  void dispose() {
+    chatController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void onClose() {
+    speechToText.stop();
+    chatController.dispose();
+    scrollController.dispose();
+    flutterTts.stop();
+    super.onClose();
+  }
+
   void sendMessage() async {
     if (chatController.text.isNotEmpty) {
-      if (file != null) {
-        final chatModel = ChatModel(
-          DateTime.now().toString(),
-          file!,
-          true,
-          true,
-        );
-        chatHistory.add(chatModel);
-        chatBox.add(chatModel);
-        file = null;
-      } else {
-        final chatModel = ChatModel(
-          DateTime.now().toString(),
-          chatController.text,
-          false,
-          true,
-        );
-        chatHistory.add(chatModel);
-        chatBox.add(chatModel);
-      }
+      String messageText = chatController.text;
+      chatController.clear();
+      update();
+      final chatModel = ChatModel(
+        DateTime.now().toString(),
+        messageText,
+        false,
+        true,
+      );
+      chatHistory.add(chatModel);
+      chatBox.add(chatModel);
       update();
       scrollController.jumpTo(scrollController.position.maxScrollExtent);
-      if (chatController.text.isNotEmpty) {
-        await getAnswer(chatController.text);
-        chatController.clear();
-      }
+      await getAnswer(messageText);
     }
   }
 
   Future<void> getAnswer(String text) async {
     try {
       late final GenerateContentResponse response;
-      var content = Content.text(text.toString());
+      var content = Content.text(text);
       response = await chat.sendMessage(content);
       if (response.text != null && response.text!.isNotEmpty) {
         final chatModel = ChatModel(
@@ -153,17 +146,16 @@ class ChatController extends GetxController {
           false,
           false,
         );
-        await systemSpeak(response.text.toString());
+        await systemSpeak(response.text.toString().replaceAll('*', ''));
         chatHistory.add(chatModel);
         chatBox.add(chatModel);
         update();
-        chatController.clear();
         scrollController.jumpTo(scrollController.position.maxScrollExtent);
       } else {
-        print("AI did not return any text.");
+        showSnackbar("AI did not return any text.");
       }
     } catch (e) {
-      print("Error sending message: $e");
+      showSnackbar(e.toString());
     }
   }
 }
--- lib/Screen/chatpage.dart
+++ lib/Screen/chatpage.dart
@@ -1,5 +1,6 @@
 import 'dart:io';
 
+import 'package:avatar_glow/avatar_glow.dart';
 import 'package:chatbot/Controller.dart/ChatController.dart';
 import 'package:chatbot/Resource/colors.dart';
 import 'package:chatbot/Utils/utils.dart';
@@ -14,151 +15,169 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          surfaceTintColor: Colors.transparent,
-          leading: InkWell(
-            onTap: () {
-              Get.back();
-            },
-            child: const Icon(
-              Icons.arrow_back,
-              color: colorPrimary,
-            ),
+      appBar: AppBar(
+        surfaceTintColor: Colors.transparent,
+        leading: InkWell(
+          onTap: () {
+Navigator.pushNamed(context, '/home');
+          },
+          child: const Icon(
+            Icons.arrow_back,
+            color: colorPrimary,
           ),
-          backgroundColor: Colors.white,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(10),
-            ),
-          ),
-          centerTitle: true,
-          title: const Text(
-            "BOB",
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                fontFamily: 'Poppins-Regular',
-                color: colorPrimary),
-          ),
-          actions: [
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Image.asset("assets/image/chatbot_icon.png",
-                  width: 50, height: 50),
-            ),
-          ],
         ),
-        body: GetBuilder<ChatController>(
-          init: ChatController(),
-          initState: (_) {},
-          builder: (chatController) {
-            return Stack(
-              children: [
-                ListView.builder(
-                  itemCount: chatController.chatHistory.length,
-                  shrinkWrap: false,
-                  controller: chatController.scrollController,
-                  padding: const EdgeInsets.only(top: 10, bottom: 80),
-                  physics: const BouncingScrollPhysics(),
-                  itemBuilder: (context, index) {
-                    final chat = chatController.chatHistory[index];
-                    return Container(
-                      padding: const EdgeInsets.all(10),
-                      child: Align(
-                        alignment: chat.isSender
-                            ? Alignment.topRight
-                            : Alignment.topLeft,
-                        child: Container(
-                          decoration: BoxDecoration(
-                            border: Border.all(
-                                color: chat.isSender
-                                    ? Colors.white
-                                    : colorPrimary),
-                            borderRadius: BorderRadius.circular(20),
-                            color: chat.isSender ? colorPrimary : Colors.white,
+        backgroundColor: Colors.white,
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(
+            bottom: Radius.circular(10),
+          ),
+        ),
+        centerTitle: true,
+        title: const Text(
+          "BOB",
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontFamily: 'Poppins-Regular',
+              color: colorPrimary),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.asset("assets/image/chatbot_icon.png",
+                width: 50, height: 50),
+          ),
+        ],
+      ),
+      body: GetBuilder<ChatController>(
+        init: ChatController(),
+        initState: (_) {},
+        builder: (chatController) {
+          return Stack(
+            children: [
+              ListView.builder(
+                itemCount: chatController.chatHistory.length,
+                shrinkWrap: false,
+                controller: chatController.scrollController,
+                padding: const EdgeInsets.only(top: 10, bottom: 80),
+                physics: const BouncingScrollPhysics(),
+                itemBuilder: (context, index) {
+                  final chat = chatController.chatHistory[index];
+                  return Container(
+                    padding: const EdgeInsets.all(10),
+                    child: Align(
+                      alignment: chat.isSender
+                          ? Alignment.topRight
+                          : Alignment.topLeft,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color:
+                                  chat.isSender ? Colors.white : colorPrimary),
+                          borderRadius: BorderRadius.circular(20),
+                          color: chat.isSender ? colorPrimary : Colors.white,
+                        ),
+                        padding: const EdgeInsets.all(16),
+                        child: Text(
+                          chat.message,
+                          style: TextStyle(
+                            fontSize: 15,
+                            color: chat.isSender ? Colors.white : Colors.black,
                           ),
-                          padding: const EdgeInsets.all(16),
-                          child: chat.isImage
-                              ? Image.file(File(chat.message), width: 200)
-                              : Text(
-                                  chat.message,
-                                  style: TextStyle(
-                                    fontSize: 15,
-                                    color: chat.isSender
-                                        ? Colors.white
-                                        : Colors.black,
-                                  ),
-                                ),
                         ),
                       ),
-                    );
-                  },
-                ),
-                Positioned(
-                  left: 12,
-                  right: 12,
-                  bottom: 12,
-                  child: TextFormField(
-                    controller: chatController.chatController,
-                    decoration: InputDecoration(
-                      hintText: "Type a message",
-                      errorBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: const BorderSide(color: colorPrimary)),
-                      enabledBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: const BorderSide(color: colorPrimary)),
-                      focusedBorder: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: const BorderSide(color: colorPrimary)),
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(30.0),
-                          borderSide: const BorderSide(color: colorPrimary)),
-                      filled: true,
-                      fillColor: Colors.white,
-                      contentPadding: const EdgeInsets.symmetric(
-                          horizontal: 20.0, vertical: 15.0),
-                      suffixIcon: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          IconButton(
-                              icon: Icon(
-                                chatController.speechToText.isListening
-                                    ? Icons.stop
-                                    : Icons.mic,
-                                color: colorPrimary,
-                              ),
-                              onPressed: () async {
-                                chatController.flutterTts.stop();
-                                if (await chatController
-                                        .speechToText.hasPermission &&
-                                    chatController
-                                        .speechToText.isNotListening) {
-                                  await chatController.startListening();
-                                } else {
-                                  chatController.initSpeechToText();
-                                }
-                              }),
-                          IconButton(
-                            icon: const Icon(
-                              Icons.send,
+                    ),
+                  );
+                },
+              ),
+              Positioned(
+                left: 12,
+                right: 12,
+                bottom: 12,
+                child: TextFormField(
+                  controller: chatController.chatController,
+                  decoration: InputDecoration(
+                    hintText: "Type a message",
+                    errorBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: const BorderSide(color: colorPrimary)),
+                    enabledBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: const BorderSide(color: colorPrimary)),
+                    focusedBorder: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: const BorderSide(color: colorPrimary)),
+                    border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                        borderSide: const BorderSide(color: colorPrimary)),
+                    filled: true,
+                    fillColor: Colors.white,
+                    contentPadding: const EdgeInsets.symmetric(
+                        horizontal: 20.0, vertical: 15.0),
+                    suffixIcon: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        IconButton(
+                            icon: Icon(
+                              chatController.speechToText.isListening
+                                  ? Icons.stop
+                                  : Icons.mic,
                               color: colorPrimary,
                             ),
-                            onPressed: () {
-                              if (chatController.chatController.text.isEmpty) {
-                                showSnackbar("Please enter a message");
+                            onPressed: () async {
+                              if (await chatController
+                                      .speechToText.hasPermission &&
+                                  chatController.speechToText.isNotListening) {
+                                await chatController.startListening();
                               } else {
-                                chatController.sendMessage();
+                                chatController.initSpeechToText();
                               }
-                            },
+                            }),
+                        IconButton(
+                          icon: const Icon(
+                            Icons.send,
+                            color: colorPrimary,
                           ),
-                        ],
+                          onPressed: () {
+                            if (chatController.chatController.text.isEmpty) {
+                              showSnackbar("Please enter a message");
+                            } else {
+                              chatController.sendMessage();
+                            }
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Positioned(
+                bottom: 80,
+                right: 12,
+                child: AvatarGlow(
+                  animate: chatController.isSpeaking,
+                  startDelay: const Duration(milliseconds: 1000),
+                  glowColor: colorPrimary,
+                  duration: const Duration(milliseconds: 2000),
+                  repeat: true,
+                  curve: Curves.fastOutSlowIn,
+                  child: const Material(
+                    elevation: 0,
+                    shape: CircleBorder(),
+                    child: CircleAvatar(
+                      backgroundColor: colorPrimary,
+                      radius: 20.0,
+                      child: Icon(
+                        Icons.volume_up,
+                        color: Colors.white,
                       ),
                     ),
                   ),
                 ),
-              ],
-            );
-          },
-        ));
+              ),
+            ],
+          );
+        },
+      ),
+    );
   }
 }
