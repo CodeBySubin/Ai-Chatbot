c1742ae1c040c13dbf8767329123c88e0b214071
subin <subin.sigosoft@gmail.com>
2024-07-20 01:12:14 +0530

    Strings added

---
+++ lib/Resource/Strings.dart
@@ -0,0 +1,9 @@
+class Strings {
+  static const String appname = "Nemo";
+  static const String writeMessage = "Write your message";
+  static const String enterMessage = 'Please enter a message';
+  static const String description = "Welcome to AI Chatbot app.\n Ask questions and receive articles on various topics.\n Features include speech to text and text to speech for\n natural and convenient interaction.";
+  static const String yourAssistant = 'Your AI Assistant';
+  static const String Continue = 'Continue';
+
+}
